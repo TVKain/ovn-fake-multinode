1fca5366c2d2668fed15bcde55c3ee59e91eb275
tvkain <tvkain.it@gmail.com>
2024-10-10 12:46:02 +0700

    fix: alias script ovn -> ovs

--- alias.sh
+++ alias.sh
@@ -3,8 +3,8 @@ central="ct-1-1"
 chassises=("gw-1", "cp-1", "cp-2")
 
 for chassis in ${chassises[@]}; do
-    alias ovn-ofctl-${chassis}="docker exec ${chassis} ovn-ofctl"
-    alias ovn-ofctl-${chassis}="docker exec ${chassis} ovn-ofctl"
+    alias ovs-ofctl-${chassis}="docker exec ${chassis} ovn-ofctl"
+    alias ovs-ofctl-${chassis}="docker exec ${chassis} ovn-ofctl"
 done
 
 alias ovn-nbctl="docker exec ${central} ovn-nbctl"
